4731c0923f0e10d70f1f4fb534e7584b3ec05f95
b-beri <91713017+b-beri@users.noreply.github.com>
2022-04-08 21:43:32 +0530

    Added Q4 Script

--- Assignment-1/Q4.sh
+++ Assignment-1/Q4.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+RED='\033[0;31m' #For Coloured Headings
+NC='\033[0m'
 
+echo "Enter the Array with space separated elements"
+read -p "-> " -a array
+
+len=${#array[*]}
+
+for ((i=0; i<$len; i++))
+do
+{
+	for ((j=0; j<$len-$i-1; j++))
+	do
+	{
+		if [ "${array[j]}" -gt "${array[$((j+1))]}" ]
+		then
+		{
+			temp=${array[$j]}
+			array[$j]=${array[$((j+1))]}
+			array[$j+1]=$temp
+		}
+		fi
+	}
+	done
+}
+done
+echo ""
+echo -e "${RED}Sorted Array is :-"
+echo ${array[*]}
